67afd8a755c3cc247b81c068d8b8efebfefe3b7e
pinkslipgg <pinkslipgg@gmail.com>
2023-06-17 18:29:11 +0530

    Update 2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
-})
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
